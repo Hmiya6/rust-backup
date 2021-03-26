438f3fcd16a221f40dbc784d51892c8c3780dab8
Hmiya6 <h.miya.work@gmail.com>
2021-03-26 19:41:41 +0900

    made main runnable

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+mod app;
+pub use app::App;
+
+pub mod config;
+// pub use config::Config;
+
+
 fn main() {
-    println!("Hello, world!");
+    let app = App::new(std::path::Path::new("test_config.toml")).unwrap();
+    app.run();
+    println!("hello world")
 }
